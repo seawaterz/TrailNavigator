4ae08224108c8f44e4aa15f7dd74357250f373f5
Muhammad Ali <Muhammad.Ali.Amin@unit4.com>
2024-10-31 23:18:19 +0100

    Add business model project

---
+++ TrailNavigator.Core.BusienssModels/DataModels/Trail/TrailEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace TrailNavigator.Core.BusienssModels.DataModels.Trail;
+
+public class TrailEntity
+{
+    public int TrailId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Difficulty { get; set; }
+    public string Length { get; set; }
+    public string Elevation { get; set; }
+    public string Status { get; set; }
+    public DateTime CreateDate { get; set; }
+}
---
+++ TrailNavigator.Core.BusienssModels/DataModels/User/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace TrailNavigator.Core.BusienssModels.DataModels.User;
+
+public class UserEntity
+{
+    public int UserId { get; set; }
+    public string Email { get; set; }
+    public string ProfileData { get; set; }
+    public string Preferences { get; set; }
+    public string ActivityHistory { get; set; }
+    public string AchivementData { get; set; }
+}
